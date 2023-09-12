5cf34b14d7fb961745a239a2549bdd9fcd263f12
Bill Fan <fanbenlong@gmail.com>
2023-09-12 14:34:36 -0400

    Hough transform working :)

--- mini_project/wall_follower.py
+++ mini_project/wall_follower.py
@@ -23,7 +23,7 @@ class WallFollowerNode(Node):
         rs = np.array(scan_msg.ranges)
         thetas = np.linspace(0, 2*np.pi, len(rs))
 
-        i_far_enough = rs > 0.1
+        i_far_enough = rs > 0.2
         rs = rs[i_far_enough]
         thetas = thetas[i_far_enough]
         
@@ -33,29 +33,18 @@ class WallFollowerNode(Node):
 
     def plot_hough(self, request, response):
         xy = self.current_scan_points
+        rho_wall, theta_wall, heatmap, extent = self.hough_tform()
 
-        ## Perform hough transform of the scan points
-        # For each point, sweep through theta-space to find the rho-values
-        n_scans = xy.shape[1]
-        n_sample_pts = 100
-        thetas = np.linspace(0, np.pi, n_sample_pts)
+        print(f"Rho wall: {rho_wall}, Theta wall: {theta_wall}")
 
-        rhos = np.zeros([n_scans, n_sample_pts])
-        for i, pt in enumerate(xy.T):
-            rhos[i, :] = pt[0] * np.sin(thetas) + pt[1] * np.cos(thetas)
-
-        thetas_all = np.matlib.repmat(thetas, 1, n_scans).flatten();
-        rhos_all = rhos.flatten()
-
-        heatmap, theta_edges, rho_edges = np.histogram2d(thetas_all, rhos_all, bins=(100, 200))
-        extent = [theta_edges[0], theta_edges[-1], rho_edges[0], rho_edges[-1]]        
-
-        i_max = np.argmax(heatmap.T)
-        rho_wall = rhos_all[i_max]
-        theta_wall = thetas_all[i_max]
+        # Plot the line we have singled out in the world space
+        x_range = [np.min(xy[0, :]), np.max(xy[0, :])]
+        wall_x = np.linspace(x_range[0], x_range[1], 100)
+        wall_y = 1/np.cos(theta_wall) * (-np.sin(theta_wall) * wall_x + rho_wall)
 
         fig, axs = plt.subplots(1, 2)
         axs[0].scatter(xy[0, :], xy[1, :])
+        axs[0].scatter(wall_x, wall_y)
         axs[0].axis('equal')
         axs[0].set_xlabel("X (m)")
         axs[0].set_ylabel("Y (m)")
@@ -70,6 +59,31 @@ class WallFollowerNode(Node):
 
         return response
 
+    def hough_tform(self):
+        xy = self.current_scan_points
+        ## Perform hough transform of the scan points
+        # For each point, sweep through theta-space to find the rho-values
+        n_scans = xy.shape[1]
+        n_sample_pts = 100
+        thetas = np.linspace(0, np.pi, n_sample_pts)
+
+        mat_rhos = np.zeros([n_scans, n_sample_pts])
+        for i, pt in enumerate(xy.T):
+            mat_rhos[i, :] = pt[0] * np.sin(thetas) + pt[1] * np.cos(thetas)
+
+        mat_thetas = np.matlib.repmat(thetas, 1, n_scans)
+
+        thetas_all = mat_thetas.flatten();
+        rhos_all = mat_rhos.flatten()
+
+        heatmap, theta_edges, rho_edges = np.histogram2d(thetas_all, rhos_all, bins=(100, 200))
+        extent = [theta_edges[0], theta_edges[-1], rho_edges[0], rho_edges[-1]]        
+
+        i_max = np.argmax(heatmap.T)
+        rho_wall = mat_rhos.flatten()[i_max]
+        theta_wall = mat_thetas.flatten()[i_max]
+
+        return rho_wall, theta_wall, heatmap, extent
 
 def main():
     rclpy.init()
